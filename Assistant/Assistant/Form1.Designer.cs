7a12e6520062723d94af082a77d156435d60c674
Cozmescu Daniel <80886420+Cozmescu-Daniel@users.noreply.github.com>
2021-06-02 12:34:35 +0300

    Final Version
    Smart assistant based on Windows functionalities.

---
+++ Assistant/Assistant/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Assistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Controls.Add(this.pictureBox3);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.pictureBox2);
+            this.panel1.Location = new System.Drawing.Point(-1, -2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1188, 664);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(468, 442);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(274, 54);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Black;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Emoji", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.button1.Location = new System.Drawing.Point(808, 550);
+            this.button1.Margin = new System.Windows.Forms.Padding(1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(76, 41);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ask";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.richTextBox1.Font = new System.Drawing.Font("Segoe UI Emoji", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.richTextBox1.Location = new System.Drawing.Point(450, 550);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(321, 41);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(410, 190);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(401, 306);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(0, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(1188, 661);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(1184, 661);
+            this.Controls.Add(this.panel1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Assistant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Assistant/Assistant/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+using System.Threading;
+using System.Net;
+using System.Diagnostics;
+using System.Speech.Recognition;
+using System.IO;
+using System.Net.NetworkInformation;
+
+namespace Assistant
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Assistant s = new Assistant();
+            pictureBox3.Visible = false;
+            s.getQ(richTextBox1.Text);
+            richTextBox1.Text = "";
+            richTextBox1.Text = s.GetR();
+
+            
+
+
+            SpeechSynthesizer synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            synth.SelectVoiceByHints(VoiceGender.Female);
+            synth.SpeakAsync(richTextBox1.Text);
+
+        }
+        
+        
+
+        private  void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+    }
+
+    public class Assistant
+    {
+        String q;
+        String r;
+        public void getQ(String q2)
+        {
+            q = q2.ToLower();
+        }
+        public String GetR()
+        {
+            if (q.Equals("hello"))
+                r = "Hi there. I'm your virtual assistant. How can I help you with?";
+            if (q.Equals("my ip"))
+            {
+                string hostName = Dns.GetHostName();
+                string myIP = Dns.GetHostByName(hostName).AddressList[0].ToString();
+                r = "Your IP Address is :" + myIP;
+            }
+            if (q.Equals("my host"))
+            {
+                string hostName = Dns.GetHostName();
+                r = "Your host name is " + hostName;
+            }
+            if (q.Equals("shutdown"))
+            {
+                Process.Start("shutdown", "/s /t 0");
+                r = "Goodbye sir";
+            }
+            if (q.Equals("hibernate"))
+            {
+                Application.SetSuspendState(PowerState.Hibernate, true, true);
+                r = "Hibernate. Got it.";
+            }
+            if (q.Equals("sleep"))
+            {
+                Application.SetSuspendState(PowerState.Suspend, true, true);
+                r = "Sleep. Got it.";
+            }
+            if (q == "open google")
+            {
+                r = "Sure. Opening Google Engine";
+                Process.Start("chrome.exe", "http://www.google.com");
+
+            }
+            if (q == "play mozart")
+            {
+                r = "Sure. Playing Mozart on Youtube";
+                Process.Start("microsoft-edge:https://www.youtube.com/watch?v=Rb0UmrCXxVA&t=357s");
+
+            }
+            if (q.Contains("search"))
+            {
+                String aux;
+                aux = q.Replace("search", " ");
+                r = "Sure, searching" + aux + " on Google";
+                System.Diagnostics.Process.Start("http://www.google.com.au/search?q=" + aux);
+            }
+            if (q.Contains("ipconfig") || q.Contains("tree") || q.Contains("cmd"))
+            {
+                System.Diagnostics.Process.Start("CMD.exe", q);
+                r = "Sure. Opening Terminal";
+            }
+            if (q.Equals("my pc"))
+            {
+                r = "Sure. Opening your pc";
+                Process.Start("::{20d04fe0-3aea-1069-a2d8-08002b30309d}");
+            }
+            if (q.Equals("i wanna code"))
+            {
+
+                Process.Start("chrome.exe", "https://stackoverflow.com");
+                Process.Start("chrome.exe", "https://github.com/");
+                Process.Start("chrome.exe", "https://java.com");
+                r = "Sure. Opening your favourite 3 tabs from Google.";
+            }
+            if (q.Equals("tell me the time") || q.Equals("tell me the date"))
+            {
+                DateTime now = DateTime.Now;
+                r = now.ToString("F");
+
+            }
+            if (q.Contains("strada"))
+            {
+                r = "Sure. Opening your location.";
+
+                Process.Start("chrome.exe", "https://www.google.ro/maps/place/" + q);
+            }
+            if (q.Equals("cpu usage"))
+            {
+                var cpuUsage = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+                Thread.Sleep(1000);
+                var firstCall = cpuUsage.NextValue();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    Thread.Sleep(1000);
+                    r = "Your cpu usage is " + cpuUsage.NextValue() + "%";
+                }
+
+            }
+            if (q.Equals("internet speed"))
+            {
+                Stopwatch stopwatch = new Stopwatch();
+
+                stopwatch.Reset();
+                stopwatch.Start();
+
+                WebClient webClient = new WebClient();
+                byte[] bytes = webClient.DownloadData("http://www.google.com");
+
+                stopwatch.Stop();
+
+                double seconds = stopwatch.Elapsed.TotalSeconds;
+
+                double speed = bytes.Count() / seconds;
+                speed = speed;
+
+                r = "Your speed is " + speed + " bytes per second.";
+            }
+            if (q.Equals("battery status"))
+            {
+
+                PowerLineStatus status = SystemInformation.PowerStatus.PowerLineStatus;
+                if (status == PowerLineStatus.Offline)
+                    r = "Running on Battery";
+                else
+                    r = "Running on Power";
+
+            }
+            if (q.Equals("wifi status"))
+            {
+                Ping myPing = new Ping();
+                String host = "google.com";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                if (reply.Status == IPStatus.Success)
+                {
+                    r = "You are online";
+                }
+                else r = "You are offline";
+            }
+            
+                return r;
+        
+        }
+    }
+
+}
